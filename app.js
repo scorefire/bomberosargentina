2086259f78dec79e8a9dd4f87056f0eed8706388
scorefire <scorefire.app@gmail.com>
2026-01-08 22:35:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,27 +2,6 @@ const URL_BOMBERO = "https://scorefire.github.io/bomberosargentina-calificacione
 const URL_JEFES   = "https://scorefire.github.io/bomberosargentina-jefes/";
 
 function entrar(tipo) {
-  const btnBombero = document.getElementById("btnBombero");
-  const btnJefe = document.getElementById("btnJefe");
-  const card = document.querySelector(".main-card");
-
-  // Deshabilitar botones
-  btnBombero.disabled = true;
-  btnJefe.disabled = true;
-
-  // Cambiar texto del botón presionado
-  if (tipo === "bombero") {
-    btnBombero.textContent = "Ingresando...";
-  } else {
-    btnJefe.textContent = "Ingresando...";
-  }
-
-  // Animación de salida
-  card.classList.add("fade-out");
-
-  // Redirección luego de la animación
-  setTimeout(() => {
-    window.location.href =
-      tipo === "bombero" ? URL_BOMBERO : URL_JEFES;
-  }, 500);
+  window.location.href =
+    tipo === "bombero" ? URL_BOMBERO : URL_JEFES;
 }
